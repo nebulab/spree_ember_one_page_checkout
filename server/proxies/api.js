18b1600b9e9d178e732e3381942387d24ff97d3d
Alessio Rocco <alessio.rocco.lt@gmail.com>
2015-02-13 16:23:31 +0100

    upgrades proxy server and setup proxy server for test

--- server/proxies/api.js
+++ server/proxies/api.js
@@ -3,18 +3,15 @@ var proxyPath = '/api';
 module.exports = function(app) {
   // For options, see:
   // https://github.com/nodejitsu/node-http-proxy
-  var Proxy = require('http-proxy')
+  var proxy = require('http-proxy').createProxyServer({});
 
-  Proxy.prototype.onError = function (err, req) {
+  proxy.on('error', function(err, req) {
     console.error(err, req.url);
-  };
-
-  var proxy = Proxy.createProxyServer({});
-  var path = require('path');
+  });
 
   app.use(proxyPath, function(req, res, next){
     // include root path in proxied request
-    req.url = path.join(proxyPath, req.url);
-    proxy.web(req, res, { target: 'http://localhost:3001/' });
+    req.url = proxyPath + '/' + req.url;
+    proxy.web(req, res, { target: 'http://localhost:3001' });
   });
 };
